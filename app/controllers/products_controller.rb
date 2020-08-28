25710cb10567ab9c8048a39cc6bf57ff28823b7d
Sabrina Robinson <sabrinarobinson@Sabrinas-MacBook-Pro.local>
2020-08-28 08:12:20 -0600

    Updates ruby versions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,5 @@
 class ProductsController < ApplicationController
   def index
-    # require 'pry' ; binding.pry
     @category =  Category.find(params[:category_id])
     @products = @category.products.includes(:variants).order(:title)
   end
